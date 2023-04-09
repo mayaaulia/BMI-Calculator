5fb662b771e2fa9d9db8a473c9468c0b27888b29
maya aulia <mayaaulia601@gmail.com>
2023-04-09 14:59:31 +0700

    add calculate function in script.js

--- script.js
+++ script.js
@@ -1,34 +1,36 @@
 const weightInput = document.querySelector(".weight-input-field");
 const heightInput = document.querySelector(".height-input-field");
-const calculateButton = document.querySelector(".calculate");
+// const calculateButton = document.querySelector(".calculate");
 const result = document.querySelector(".bmi-result");
 const statement = document.querySelector(".result-category");
 let BMI = 0;
 let weight = 0;
 let height= 0;
+function calculate (){
+    // console.log ("test")
+    weight = weightInput.value;
+    // console.log(weight)
+    height = heightInput.value;
+    // console.log(height)
+    let changetoMeter = height/100
+    BMI = weight/(changetoMeter**2);
+    let fixedBMI = BMI.toFixed(1) 
+    // console.log(BMI)
+    result.innerText = fixedBMI;
+    
+    //category condition
+    if(BMI < 18.5){
+        statement.innerText = "Underwight"
+    }else if((BMI > 18.5) && (BMI <= 24.9)){
+        statement.innerText = "Normal"
+    }else if((BMI >= 25) && (BMI <= 29.9) ){
+        statement.innerText = "Overweight"
+    }else if( BMI >= 30){
+        statement.innerText = "Obesity"  
+    }
 
-calculateButton.addEventListener("click", () =>{
-// console.log ("test")
-weight = weightInput.value;
-// console.log(weight)
-height = heightInput.value;
-// console.log(height)
-let changetoMeter = height/100
-BMI = weight/(changetoMeter**2);
-let fixedBMI = BMI.toFixed(1) 
-// console.log(BMI)
-result.innerText = fixedBMI;
-
-//category condition
-if(BMI < 18.5){
-    statement.innerText = "Underwight"
-}else if((BMI > 18.5) && (BMI <= 24.9)){
-    statement.innerText = "Normal"
-}else if((BMI >= 25) && (BMI <= 29.9) ){
-    statement.innerText = "Overweight"
-}else if( BMI >= 30){
-    statement.innerText = "Obesity"  
 }
+// calculateButton.addEventListener("click", () =>{
 
 // switch(BMI){
 //     case BMI < 18.5:
@@ -44,4 +46,4 @@ if(BMI < 18.5){
 //         statement.innerHTML = "Obesity" 
 //     break;    
 // }
-});
+// });
